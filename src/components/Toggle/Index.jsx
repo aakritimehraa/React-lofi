3132d1d54ffce63704534774bc928f3a52e1e7ca
Codesis-Aakriti <aakriti@codesis.tech>
2024-01-01 02:18:33 +0530

    fix deploy er

--- src/components/Toggle/Index.jsx
+++ src/components/Toggle/Index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Style.scss'
 import { useDispatch, useSelector } from 'react-redux';
 import { toggleMode } from '../../reducers/toggleSlice';
