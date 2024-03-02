4efe4b5ac5229819e1af1178babe8232a603bf7d
Codesis-Aakriti <aakriti@codesis.tech>
2024-03-02 22:28:51 +0530

    update links and css

--- src/components/BtnsBoard/Index.jsx
+++ src/components/BtnsBoard/Index.jsx
@@ -156,6 +156,8 @@ const TodoBoard = ({ handleOpen }) => {
   return (
     <div className='board'>
       <h2 className='board_title'>Todo</h2>
+
+      <p style={{color:'#ffffff' , display:'flex' , justifyContent:'center'}}>Coming soon!</p>
     </div>
   )
 }
